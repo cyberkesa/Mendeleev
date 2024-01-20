d70ff77265829a90de3a253b24b0a27573aaca64
cyberkesa <nastya63rus@mail.ru>
2024-01-20 17:10:19 +0300

    Add Colors blocks

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,38 +1,42 @@
 import React, { useState } from "react";
-import "../Element.css";
+import "../Colors.css";
 
 const Colors = () => {
   return (
-    <div className="colors-table">
-      <span className="q-alkali-metals"></span>
-      <span className="alkali-metals">alkali metals</span>
+    <div className="colors">
+      <div className="colors-table">
+        <div className="q q-alkali-metals"></div>
+        <span className="alkali-metals">alkali metals</span>
 
-      <span className="q-alkaline"></span>
-      <span className="alkaline">alkaline earth metals</span>
+        <div className="q q-alkaline"></div>
+        <span className="alkaline">alkaline earth metals</span>
 
-      <span className="q-lanthanides"></span>
-      <span className="lanthanides">lanthanides</span>
+        <div>
+          <div className="q q-lanthanides"></div>
+          <span className="lanthanides">lanthanides</span>
 
-      <span className="q-actinides"></span>
-      <span className="actinides">actinides</span>
+          <div className="q q-actinides"></div>
+          <span className="actinides">actinides</span>
+        </div>
 
-      <span className="q-actinides"></span>
-      <span className="actinides">transition metals</span>
+        <div className="q q-actinides"></div>
+        <span className="actinides">transition metals</span>
 
-      <span className="q-unknown"></span>
-      <span className="unknown">unknown properties</span>
+        <div className="q q-unknown"></div>
+        <span className="unknown">unknown properties</span>
 
-      <span className="q-post"></span>
-      <span className="post">post-transition metals</span>
+        <div className="q q-post"></div>
+        <span className="post">post-transition metals</span>
 
-      <span className="q-metalloids"></span>
-      <span className="metalloids">metalloids</span>
+        <div className="q q-metalloids"></div>
+        <span className="metalloids">metalloids</span>
 
-      <span className="q-reactive"></span>
-      <span className="reactive">reactive nonmetals</span>
+        <div className="q q-reactive"></div>
+        <span className="reactive">reactive nonmetals</span>
 
-      <span className="q-noble"></span>
-      <span className="noble">noble gases</span>
+        <div className="q q-noble"></div>
+        <span className="noble">noble gases</span>
+      </div>
     </div>
   );
 };
