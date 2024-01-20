71294bc918d0b4dfafd5bd61237d79c80fa22d48
cyberkesa <nastya63rus@mail.ru>
2024-01-20 16:47:03 +0300

    Add Colors text

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,5 +1,40 @@
 import React, { useState } from "react";
+import "../Element.css";
 
-const Colors = () => {};
+const Colors = () => {
+  return (
+    <div className="colors-table">
+      <span className="q-alkali-metals"></span>
+      <span className="alkali-metals">alkali metals</span>
+
+      <span className="q-alkaline"></span>
+      <span className="alkaline">alkaline earth metals</span>
+
+      <span className="q-lanthanides"></span>
+      <span className="lanthanides">lanthanides</span>
+
+      <span className="q-actinides"></span>
+      <span className="actinides">actinides</span>
+
+      <span className="q-actinides"></span>
+      <span className="actinides">transition metals</span>
+
+      <span className="q-unknown"></span>
+      <span className="unknown">unknown properties</span>
+
+      <span className="q-post"></span>
+      <span className="post">post-transition metals</span>
+
+      <span className="q-metalloids"></span>
+      <span className="metalloids">metalloids</span>
+
+      <span className="q-reactive"></span>
+      <span className="reactive">reactive nonmetals</span>
+
+      <span className="q-noble"></span>
+      <span className="noble">noble gases</span>
+    </div>
+  );
+};
 
 export default Colors;
