070ac76a56b042ebcc849677b176c3ceb7826c03
cyberkesa <nastya63rus@mail.ru>
2024-01-22 00:51:22 +0300

    Add Electron Configuration table with Notes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import PeriodicTable from "./components/PeriodicTable";
 import Colors from "./components/Colors";
 import Element from "./components/Element";
+import ElectronConfiguration from "./components/ElectronConfiguration";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <main>
         <Element />
         <PeriodicTable />
+        <ElectronConfiguration />
         <Colors />
       </main>
     </div>
---
+++ src/components/ElectronConfiguration.js
@@ -0,0 +1,15 @@
+import "../ElectronConfiguration.css";
+
+const ElectronConfiguration = () => {
+  return (
+    <div className="e-conf-table">
+      <h4>Notes</h4>
+      <ul className="notes-li">
+        <li>1 kj/mol 96.485 eV</li>
+        <li>all elements are implied to have an oxidarion state zero</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ElectronConfiguration;
