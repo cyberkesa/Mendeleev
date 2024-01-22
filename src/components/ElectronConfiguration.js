5a2c6958c205ac341654cc3f84651f19097efe58
cyberkesa <nastya63rus@mail.ru>
2024-01-22 03:16:17 +0300

    Add table electron configuration blocs

--- src/components/ElectronConfiguration.js
+++ src/components/ElectronConfiguration.js
@@ -3,6 +3,21 @@ import "../ElectronConfiguration.css";
 const ElectronConfiguration = () => {
   return (
     <div className="e-conf-table">
+      <div className="mini-table">
+        <div className="div1">s</div>
+        <div className="div2"></div>
+        <div className="div3"></div>
+        <div className="div4">
+          <span className="letter-notes">d</span>
+        </div>
+        <div className="div5">
+          <span className="letter-notes">p</span>
+        </div>
+        <div className="div6"></div>
+        <div className="div7">
+          <span className="letter-notes">f</span>
+        </div>
+      </div>
       <h4 className="notes">Notes</h4>
       <ul className="notes-li">
         <li>1 kj/mol 96.485 eV</li>
